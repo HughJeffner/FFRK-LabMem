f8eecae141085fb4f238ad5d68e5129254ff29c7
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2021-06-23 19:49:50 +0000

    State machine startup state

--- FFRK-LabMem/Program.cs
+++ FFRK-LabMem/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SharpAdbClient;
-using System.Drawing;
 
 namespace FFRK_LabMem
 {
@@ -12,12 +11,6 @@ namespace FFRK_LabMem
     {
         static void Main(string[] args)
         {
-
-           
-
-
-
-
 
             AdbServer server = new AdbServer();
             var result = server.StartServer(@"C:\Users\JLingis\SDK\android-sdk\platform-tools\adb.exe", restartServerIfNewer: false);
@@ -27,20 +20,14 @@ namespace FFRK_LabMem
             if (device != null)
             {
                 Console.WriteLine("Connected to " + device.Name);
+                var lab = new Lab(device);
+               
             }
             else
             {
                 Console.WriteLine("Could not connect");
             }
-      
-            using (var framebuffer = AdbClient.Instance.GetFrameBufferAsync(device, System.Threading.CancellationToken.None).Result){
-                using (Bitmap b = new Bitmap(framebuffer))
-                {
-                    Console.WriteLine(b.GetPixel(100,200).ToString());
-                }
-                
-            }
-                       
+             
             Console.ReadKey();
             
         }
