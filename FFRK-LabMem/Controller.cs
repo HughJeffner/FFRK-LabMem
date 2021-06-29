b4ec7c74cfc1b8e828932c19c4e380c7b1439811
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2021-06-29 20:51:57 +0000

    Starting to come together

---
+++ FFRK-LabMem/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFRK_LabMem.Services;
+
+namespace FFRK_LabMem
+{
+    class Controller
+    {
+
+        public Lab Lab { get; set; }
+        public Proxy Proxy { get; set; }
+
+        public Controller(Lab lab, Proxy proxy)
+        {
+            this.Lab = lab;
+            this.Proxy = proxy;
+            this.Proxy.OnPaintingsLoaded += Proxy_OnPaintingsLoaded;
+
+        }
+
+        void Proxy_OnPaintingsLoaded(Newtonsoft.Json.Linq.JArray paintings)
+        {
+            Lab.OnPaintingsLoaded(paintings);
+        }
+        
+    }
+}
---
+++ FFRK-LabMem/Services/Adb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpAdbClient;
+
+namespace FFRK_LabMem.Services
+{
+    class Adb
+    {
+
+        public DeviceData Device { get; set; }
+
+        public Adb()
+        {
+
+            AdbServer server = new AdbServer();
+            var result = server.StartServer(@"adb.exe", restartServerIfNewer: false);
+            Console.WriteLine("Adb status: {0}", result);
+
+        }
+
+        public bool Connect()
+        {
+            //AdbClient.Instance.Connect(new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 7555));
+            AdbClient.Instance.Connect("127.0.0.1:7555");
+            this.Device = AdbClient.Instance.GetDevices().FirstOrDefault();
+            if (this.Device != null && this.Device.State == DeviceState.Online)
+            {
+                Console.WriteLine("Connected to " + this.Device.Name);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Could not connect");
+                return false;
+            }
+
+        }
+    }
+}
