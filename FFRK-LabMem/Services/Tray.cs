258caf950baf6f8685c23af3d8dc90aecf1eb894
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2021-07-06 18:34:41 +0000

    Update tray service

--- FFRK-LabMem/Services/Tray.cs
+++ FFRK-LabMem/Services/Tray.cs
@@ -25,24 +25,20 @@ namespace FFRK_LabMem.Services
 
         public static void MinimizeTo(){
             
+            // Windows API to hide window
             ShowWindow(GetConsoleWindow(), SW_HIDE);
 
+            // Init tray icon from windows forms
             if (notifyIcon == null)
             {
 
                 notifyIcon = new NotifyIcon();
-                notifyIcon.DoubleClick += (s, e) =>
-                {
-                    ShowWindow(GetConsoleWindow(), SW_SHOW);
-                    notifyIcon.Visible = false;
-                    Application.Exit();
-                };
-                
+                notifyIcon.DoubleClick +=notifyIcon_DoubleClick;
                 notifyIcon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
                 notifyIcon.Text = Application.ProductName;
 
                 var contextMenu = new ContextMenuStrip();
-                contextMenu.Items.Add("Exit", null, (s, e) => { Application.Exit(); });
+                contextMenu.Items.Add("Show", null, (s, e) => { notifyIcon_DoubleClick(s, e); });
                 notifyIcon.ContextMenuStrip = contextMenu;
 
                 notifyIcon.Visible = true;
@@ -53,11 +49,20 @@ namespace FFRK_LabMem.Services
                 notifyIcon.Visible = true;
             }
 
+            // Message pump to handle icon events - console will pause
             Application.Run(); 
 
+        }
+
+        private static void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
             
+            // Windows API to show window
+            ShowWindow(GetConsoleWindow(), SW_SHOW);
+            notifyIcon.Visible = false;
 
-
+            // Stop message pump, resume console
+            Application.Exit();
         }
 
     }
