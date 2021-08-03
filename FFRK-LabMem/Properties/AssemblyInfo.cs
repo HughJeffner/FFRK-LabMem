27316cb8a4f2f93a9a94e3c905c8746bc1562ce2
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2021-08-03 12:47:52 -0500

    Update version, AssemblyInfo

--- FFRK-LabMem/Properties/AssemblyInfo.cs
+++ FFRK-LabMem/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FFRK-LabMem")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Labyrinth auto macro for FFRK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HughJeffner")]
 [assembly: AssemblyProduct("FFRK-LabMem")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyCopyright("Copyright © HughJeffner 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.10.0")]
-[assembly: AssemblyFileVersion("0.9.10.0")]
+[assembly: AssemblyVersion("0.9.11.0")]
+[assembly: AssemblyFileVersion("0.9.11.0")]
