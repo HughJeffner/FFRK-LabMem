15f38232a7324703cd51c2af7afc443216e8cda2
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2021-08-27 09:58:04 -0500

    Added shortcut to copy proxy bypass to clipboard

--- FFRK-LabMem/Program.cs
+++ FFRK-LabMem/Program.cs
@@ -9,7 +9,6 @@ namespace FFRK_LabMem
 {
     class Program
     {
-
         static void Main(string[] args)
         {
 
@@ -45,6 +44,7 @@ namespace FFRK_LabMem
                 if (key.Key == ConsoleKey.H) Tray.MinimizeTo(key.Modifiers);
                 if (key.Key == ConsoleKey.U && key.Modifiers == ConsoleModifiers.Alt) Updates.OpenReleasesInBrowser("hughjeffner", "ffrk-labmem");
                 if (key.Key == ConsoleKey.O && key.Modifiers == ConsoleModifiers.Alt) controller.AutoDetectOffsets(config);
+                if (key.Key == ConsoleKey.B && key.Modifiers == ConsoleModifiers.Control) Clipboard.CopyProxyBypassToClipboard();
             }
             
             // Stop
---
+++ FFRK-LabMem/Services/Clipboard.cs
@@ -0,0 +1,25 @@
+﻿using FFRK_Machines;
+using System;
+using System.Threading;
+
+namespace FFRK_LabMem.Services
+{
+
+    class Clipboard
+    {
+        private const string PROXY_BYPASS = "127.0.0.1,lcd-prod.appspot.com,live.chartboost.com,android.clients.google.com,googleapis.com,ssl.sp.mbga-platform.jp,ssl.sp.mbga.jp,app.adjust.io";
+
+        public static void CopyProxyBypassToClipboard()
+        {
+
+            Thread thread = new Thread(() => {
+                System.Windows.Forms.Clipboard.SetText(PROXY_BYPASS);
+                ColorConsole.WriteLine(ConsoleColor.Gray, "Copied proxy bypass to clipboard");
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+        }
+
+    }
+}
