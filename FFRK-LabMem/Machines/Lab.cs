2d019cf918226798003fd93febc43e7b3e03eb13
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2022-03-03 14:52:02 -0600

    Prevent navigate back when in Battle state

--- FFRK-LabMem/Machines/Lab.cs
+++ FFRK-LabMem/Machines/Lab.cs
@@ -305,6 +305,7 @@ namespace FFRK_LabMem.Machines
                     ColorConsole.WriteLine(ConsoleColor.DarkGray, "Navigating back");
                     for (int i = 0; i < 3; i++)
                     {
+                        if (StateMachine.State == State.Battle) break;
                         await Adb.NavigateBack(this.CancellationToken);
                         await Task.Delay(500);
                     }
