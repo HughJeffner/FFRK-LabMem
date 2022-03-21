c6c76d555ce77456d2dc71dc782be3b7a551d243
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2022-03-21 12:39:38 -0500

    Allow drag and drop re-order of enemy priority list

--- FFRK-LabMem/Config/UI/ConfigForm.cs
+++ FFRK-LabMem/Config/UI/ConfigForm.cs
@@ -1170,5 +1170,78 @@ namespace FFRK_LabMem.Config.UI
                 textBoxScreenshotFolder.Text = folderBrowserDialog1.SelectedPath;
             }
         }
+
+        #region DragDropRegion
+        // https://stackoverflow.com/a/1623968/383761
+        private Rectangle dragBoxFromMouseDown;
+        private int rowIndexFromMouseDown;
+        private int rowIndexOfItemUnderMouseToDrop;
+        private void DataGridViewEnemies_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                // If the mouse moves outside the rectangle, start the drag.
+                if (dragBoxFromMouseDown != Rectangle.Empty &&
+                    !dragBoxFromMouseDown.Contains(e.X, e.Y))
+                {
+
+                    // Proceed with the drag and drop, passing in the list item.                    
+                    DragDropEffects dropEffect = dataGridViewEnemies.DoDragDrop(
+                    dataGridViewEnemies.Rows[rowIndexFromMouseDown],
+                    DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void DataGridViewEnemies_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Get the index of the item the mouse is below.
+            rowIndexFromMouseDown = dataGridViewEnemies.HitTest(e.X, e.Y).RowIndex;
+            if (rowIndexFromMouseDown != -1 && rowIndexFromMouseDown < dataGridViewEnemies.Rows.Count - 1)
+            {
+                // Remember the point where the mouse down occurred. 
+                // The DragSize indicates the size that the mouse can move 
+                // before a drag event should be started.                
+                Size dragSize = SystemInformation.DragSize;
+
+                // Create a rectangle using the DragSize, with the mouse position being
+                // at the center of the rectangle.
+                dragBoxFromMouseDown = new Rectangle(new Point(e.X - (dragSize.Width / 2),
+                                                               e.Y - (dragSize.Height / 2)),
+                                    dragSize);
+            }
+            else
+                // Reset the rectangle if the mouse is not over an item in the ListBox.
+                dragBoxFromMouseDown = Rectangle.Empty;
+        }
+
+        private void DataGridViewEnemies_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void DataGridViewEnemies_DragDrop(object sender, DragEventArgs e)
+        {
+            // The mouse locations are relative to the screen, so they must be 
+            // converted to client coordinates.
+            Point clientPoint = dataGridViewEnemies.PointToClient(new Point(e.X, e.Y));
+
+            // Get the row index of the item the mouse is below. 
+            rowIndexOfItemUnderMouseToDrop = dataGridViewEnemies.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+            // If the drag operation was a move then remove and insert the row.
+            if (e.Effect == DragDropEffects.Move)
+            {
+                DataGridViewRow rowToMove = e.Data.GetData(typeof(DataGridViewRow)) as DataGridViewRow;
+
+                dataGridViewEnemies.Rows.RemoveAt(rowIndexFromMouseDown);
+                if (rowIndexOfItemUnderMouseToDrop >= dataGridViewEnemies.Rows.Count) { rowIndexOfItemUnderMouseToDrop--; }
+                dataGridViewEnemies.Rows.Insert(rowIndexOfItemUnderMouseToDrop, rowToMove);
+            }
+        }
+
+        #endregion
+
     }
+
 }
