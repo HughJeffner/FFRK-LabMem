bcc1b6664bb046c6b5e421c5bfbe60e154b1bde5
Hugh Jeffner <HughJeffner@users.noreply.github.com>
2022-05-25 12:42:31 -0500

    Added error handling when loading config file

--- FFRK-LabMem/Config/AppConfig.cs
+++ FFRK-LabMem/Config/AppConfig.cs
@@ -12,14 +12,16 @@ namespace FFRK_LabMem.Config
     public class AppConfig
     {
         static ConfigHelper helper;
-        public class ConsoleConfig
+        private List<IAppConfigSection> configs;
+
+        public class ConsoleConfig : IAppConfigSection
         {
             public bool Timestamps { get; set; } = true;
             public ColorConsole.DebugCategory DebugCategories { get; set; } = 0;
             public bool Logging { get; set; } = false;
             public String LogFolder { get; set; } = "";
             public int BufferSize { get; set; } = 10;
-            public ConsoleConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 Timestamps = helper.GetBool("console.timestamps", true);
                 DebugCategories = (ColorConsole.DebugCategory)helper.GetInt("console.debugCategories", 0);
@@ -40,11 +42,11 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class UpdateConfig
+        public class UpdateConfig : IAppConfigSection
         {
             public bool CheckForUpdates { get; set; } = false;
             public bool IncludePrerelease { get; set; } = false;
-            public UpdateConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 CheckForUpdates = helper.GetBool("updates.checkForUpdates", false);
                 IncludePrerelease = helper.GetBool("updates.includePrerelease", false);
@@ -59,11 +61,11 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class ScreenConfig
+        public class ScreenConfig : IAppConfigSection
         {
             public int TopOffset { get; set; } = -1;
             public int BottomOffset { get; set; } = -1;
-            public ScreenConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 TopOffset = helper.GetInt("screen.topOffset", -1);
                 BottomOffset = helper.GetInt("screen.bottomOffset", -1);
@@ -78,7 +80,7 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class AdbConfig
+        public class AdbConfig : IAppConfigSection
         {
             public bool CloseOnExit { get; set; } = false;
             public string Path { get; set; } = "adb.exe";
@@ -92,7 +94,7 @@ namespace FFRK_LabMem.Config
             public int TapDuration { get; set; } = 0;
             public int TapPressure { get; set; } = 50;
             public String ScreenshotFolder { get; set; } = "";
-            public AdbConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 CloseOnExit = helper.GetBool("adb.closeOnExit", false);
                 Path = helper.GetString("adb.path", "adb.exe");
@@ -127,10 +129,10 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class DataLogConfig
+        public class DataLogConfig : IAppConfigSection
         {
             public bool Enabled { get; set; } = false;
-            public DataLogConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 Enabled = helper.GetBool("datalogger.enabled", false);
             }
@@ -140,12 +142,12 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class CountersConfig
+        public class CountersConfig : IAppConfigSection
         {
             public Counters.DropCategory DropCategories { get; set; } = (Counters.DropCategory)15;
             public bool LogDropsToTotalCounters { get; set; } = false;
             public int MaterialsRarityFilter { get; set; } = 6;
-            public CountersConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 DropCategories = (Counters.DropCategory)helper.GetInt("counters.dropCategories", 15);
                 LogDropsToTotalCounters = helper.GetBool("counters.logDropsToTotal", false);
@@ -162,7 +164,7 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class LabConfig
+        public class LabConfig : IAppConfigSection
         {
             public string ConfigFile { get; set; } = "config/lab.balanced.json";
             public int WatchdogHangSeconds { get; set; } = 120;
@@ -175,7 +177,7 @@ namespace FFRK_LabMem.Config
             public int WatchdogRestartLoopThreshold { get; set; } = 6;
             public int WatchdogRestartLoopWindowMinutes { get; set; } = 60;
             public int WatchdogBattleMaxRetries { get; set; } = 5;
-            public LabConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 ConfigFile = helper.GetString("lab.configFile", "config/lab.balanced.json");
                 WatchdogHangSeconds = helper.GetInt("lab.watchdogHangSeconds", 120);
@@ -208,14 +210,14 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class ProxyConfig
+        public class ProxyConfig : IAppConfigSection
         {
             public int Port { get; set; } = 8081;
             public bool Secure { get; set; } = true;
             public string Blocklist { get; set; } = "";
             public bool ConnectionPooling { get; set; } = false;
             public bool AutoConfig { get; set; } = false;
-            public ProxyConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 Port = helper.GetInt("proxy.port", 8081);
                 Secure = helper.GetBool("proxy.secure", false);
@@ -236,10 +238,10 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public class SchedulerConfig
+        public class SchedulerConfig : IAppConfigSection
         {
             public int MaintenanceDoneHourUTC { get; set; } = 13;
-            public SchedulerConfig(ConfigHelper helper)
+            public void Load(ConfigHelper helper)
             {
                 MaintenanceDoneHourUTC = helper.GetInt("scheduler.maintenanceDoneHourUtc", 13);
             }
@@ -252,41 +254,51 @@ namespace FFRK_LabMem.Config
             }
         }
 
-        public ConsoleConfig Console { get; private set; }
-        public UpdateConfig Updates { get; private set; }
-        public ScreenConfig Screen { get; private set; }
-        public AdbConfig Adb { get; private set; }
-        public DataLogConfig DataLog { get; private set; }
-        public CountersConfig Counters { get; private set; }
-        public LabConfig Lab { get; private set; }
-        public ProxyConfig Proxy { get; private set; }
-        public SchedulerConfig Scheduler { get; private set; }
+        public ConsoleConfig Console { get; private set; } = new ConsoleConfig();
+        public UpdateConfig Updates { get; private set; } = new UpdateConfig();
+        public ScreenConfig Screen { get; private set; } = new ScreenConfig();
+        public AdbConfig Adb { get; private set; } = new AdbConfig();
+        public DataLogConfig DataLog { get; private set; } = new DataLogConfig();
+        public CountersConfig Counters { get; private set; } = new CountersConfig();
+        public LabConfig Lab { get; private set; } = new LabConfig();
+        public ProxyConfig Proxy { get; private set; } = new ProxyConfig();
+        public SchedulerConfig Scheduler { get; private set; } = new SchedulerConfig();
 
         public AppConfig(String configFile)
         {
-            helper = new ConfigHelper(configFile);
-            Console = new ConsoleConfig(helper);
-            Updates = new UpdateConfig(helper);
-            Screen = new ScreenConfig(helper);
-            Adb = new AdbConfig(helper);
-            DataLog = new DataLogConfig(helper);
-            Counters = new CountersConfig(helper);
-            Lab = new LabConfig(helper);
-            Proxy = new ProxyConfig(helper);
-            Scheduler = new SchedulerConfig(helper);
+
+            configs = new List<IAppConfigSection>()
+            {
+                Console,
+                Updates,
+                Screen,
+                Adb,
+                DataLog,
+                Counters,
+                Lab,
+                Proxy,
+                Scheduler
+            };
+
+            try
+            {
+                helper = new ConfigHelper(configFile);
+                foreach (var item in configs)
+                {
+                    item.Load(helper);
+                }
+            } catch (Exception e)
+            {
+                ColorConsole.WriteLine(ConsoleColor.Yellow, $"Error loading config file, using default values: {e}");
+            }
         }
 
         public void SaveAll()
         {
-            Console.Save();
-            Updates.Save();
-            Screen.Save();
-            Adb.Save();
-            DataLog.Save();
-            Counters.Save();
-            Lab.Save();
-            Proxy.Save();
-            Scheduler.Save();
+            foreach (var item in configs)
+            {
+                item.Save();
+            }
         }
 
     }
